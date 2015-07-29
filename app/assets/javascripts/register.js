3af8e88da9a1313292c502e8cbc28db38a0bd972
Jeff Shapiro <codesheff@gmail.com>
2015-07-29 14:48:43 -0400

    js work on privacy policy

--- app/assets/javascripts/register.js
+++ app/assets/javascripts/register.js
@@ -1,1 +1,12 @@
-register.js
+$(document).ready(function() {
+
+
+$('#lock-pic').unbind('mouseenter').bind('mouseenter',function(){
+  console.log('open lock');
+});
+
+$('#lock-pic').unbind('mouseleave').bind('mouseleave',function(){
+  console.log('close lock');
+});
+
+});
