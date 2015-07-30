0bfe9d3817036a81a6b3aa3e03e5b9e26cb3b318
Jeff Shapiro <codesheff@gmail.com>
2015-07-30 19:37:32 -0400

    tutorial scene I to II transition

--- app/assets/javascripts/tutorial.js
+++ app/assets/javascripts/tutorial.js
@@ -4,13 +4,28 @@
 
 
 $(document).ready(function() {
+  
+  
 
-  window.setTimeout(function() {
-    $('#getstart_inside').animate({opacity:1,});
-  }, 5000);
 
-  $('#thanks_banner').animate({width: 400, "left":"-1%",}, 2000);
-  $('#getstart_banner').delay(4000).animate({width: 500, "right":"-35%",}, 2000);
+  // SCENE I to SCENE II TRANSITION
+
+  $('#step_1_arrow').click(function() {
+
+    window.setTimeout(function() {
+      $('#getstart_banner').animate({"right":"100%"}, 2800);
+      $('#thanks_banner').animate({"left":"-100%"}, 4000);
+      $('#scene1').delay(2000).animate({"right":"100%"}, 1);
+      $('#scene2').delay(2000).animate({"left":"0%"}, 1);
+
+    },1);
+
+    
+    
+
+
+
+  });
 
 
 
