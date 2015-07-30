9d78a67b4f2975f3efa806f2eec929c67dddef13
Jeff Shapiro <codesheff@gmail.com>
2015-07-30 19:44:25 -0400

    more transition I to II work

--- app/assets/javascripts/tutorial.js
+++ app/assets/javascripts/tutorial.js
@@ -13,8 +13,8 @@ $(document).ready(function() {
   $('#step_1_arrow').click(function() {
 
     window.setTimeout(function() {
-      $('#getstart_banner').animate({"right":"100%"}, 2800);
-      $('#thanks_banner').animate({"left":"-100%"}, 4000);
+      $('#getstart_banner').animate({"right":"100%"}, 2300);
+      $('#thanks_banner').animate({"left":"-100%"}, 3000);
       $('#scene1').delay(2000).animate({"right":"100%"}, 1);
       $('#scene2').delay(2000).animate({"left":"0%"}, 1);
 
