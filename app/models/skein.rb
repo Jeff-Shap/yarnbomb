42ce06ab447df70e56655b6c32b8924ee5c649e0
Jeff Shapiro <codesheff@gmail.com>
2015-08-04 20:37:59 -0400

    delete skein model, prep to create skein scaffold

--- app/models/skein.rb
+++
@@ -1,3 +0,0 @@
-class Skein < ActiveRecord::Base
-  belongs_to :yarn
-end
--- db/migrate/20150609194633_create_skeins.rb
+++
@@ -1,9 +0,0 @@
-class CreateSkeins < ActiveRecord::Migration
-  def change
-    create_table :skeins do |t|
-      t.integer :yardage
-
-      t.timestamps null: false
-    end
-  end
-end
